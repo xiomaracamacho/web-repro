f83646c8d8ad3966dd6213d2d1c5d04cc3515a51
xiomaracamacho <xiomaracamacho0526@gmail.com>
2023-12-02 12:44:57 -0500

    merCAA

--- mercadoLibre.js
+++ mercadoLibre.js
@@ -1,132 +1,144 @@
 let inputBusqueda = document.getElementById("inputBusqueda");
-
 class Automovil {
-    constructor(marca, modelo, precio, imagen, fecha, kilometraje, ubicacion) {
+    constructor(marca, modelo, precio, ao, kilometros, ciudad, imagen) {
         this.marca = marca;
         this.modelo = modelo;
         this.precio = precio;
+        this.ao = ao;
+        this.kilometros = kilometros;
+        this.ciudad = ciudad;
         this.imagen = imagen;
-        this.fecha = fecha;
-        this.kilometraje = kilometraje;
-        this.ubicacion = ubicacion;
-        this.favorito = false; // Inicialmente, el automóvil no es un favorito
     }
 }
 
-let autos = [
-    new Automovil("Chevrolet", "Fox", 115000000, "img/chevrolet.avif", "2023", "10.000 km", "Bogotá - Colombia"),
-    new Automovil("Toyota", "TXL", 75000000, "img/Toyota.png", "2023", "30.000 km", "Cali - Colombia"),
-    new Automovil("Mitsubishi", "4x4", 95000000, "img/mishu.jpg", "2022", "70.000 km", "Medellín - Colombia"),
-    new Automovil("Mitsubishi", "Full", 120000000, "img/mitsubishi1.jpg", "2021", "20.000 km", "Cali - Colombia"),
-    new Automovil("Mazda", "2023", 135000000, "img/mazda.jpg", "2022", "30.000 km", "Bogotá - Colombia"),
-    new Automovil("Mazda", "2024", 145000000, "img/mazda1.jpg", "2023", "15.000 km", "Medellín - Colombia"),
-    new Automovil("Mazda", "Corolla", 65000000, "img/mazda.jpg", "2022", "80.000 km", "Bogotá - Colombia")
-];
+let auto1 = new Automovil("Toyota", "TXL", 20000000, "2023", "0", "Pitalito-Huila", "img/Toyota.png");
+let auto2 = new Automovil("Mazda", "Full", 3000000, 2022, 0, "Neiva-Huila", "img/mazda1.jpg");
+let auto3 = new Automovil("Mitsubishi", "4x4", 40000000, 2023, 0, "Bogotá", "img/mishu.jpg");
+let auto4 = new Automovil("Chevrolet", "Full", 90000000, 2023, 0, "Neiva-Huila", "img/chevrolet.avif");
+let auto5 = new Automovil("Mazda", "Familiar", 10000000, 2024, 0, "Bogotá", "img/mazda.jpg");
+/* 
 
-// Función para mostrar los productos que coinciden con la búsqueda
-function mostrarProductos(terminoBusqueda) {
-    let mainContent = document.getElementById("mainContent");
-    mainContent.innerHTML = ''; // Limpiar el contenido principal antes de mostrar resultados
+/* Función  para cargar el vehículo*/
+cargarVehiculo(auto1);
+cargarVehiculo(auto2);
+cargarVehiculo(auto3);
+cargarVehiculo(auto4);
+cargarVehiculo(auto5);
 
-    autos.forEach(auto => {
-        if (auto.marca.toLowerCase().includes(terminoBusqueda.toLowerCase()) || auto.modelo.toLowerCase().includes(terminoBusqueda.toLowerCase())) {
-            // Código para mostrar el producto según los criterios de búsqueda
-            let boxproducto = document.createElement("div");
-            mainContent.appendChild(boxproducto);
-            boxproducto.setAttribute("class", "box-producto");
+window.addEventListener("keydown", function(event) {
+    let busqueda = document.getElementById("inputBusqueda").value;
+    if (event.key == "Enter") {
+        boxProductos.innerHTML = "";
+        let inputUser = busqueda.toLowerCase();
+        if (inputUser == "mazda") {
+            cargarVehiculo(auto2);
+            cargarVehiculo(auto5);
+        }  
+        else if (inputUser == "toyota") {
+            cargarVehiculo(auto1);
+        } 
+        else if (inputUser == "mitsubishi") {
+            cargarVehiculo(auto3);
 
-            let boximg = document.createElement("div");
-            boxproducto.appendChild(boximg);
-            boximg.setAttribute("class", "box-img");
-
-            let imgAuto = document.createElement("img");
-            boximg.appendChild(imgAuto);
-            imgAuto.setAttribute("src", auto.imagen);
-            imgAuto.setAttribute("class", "img-auto");
-
-            let boxInfo = document.createElement("div");
-            boxproducto.appendChild(boxInfo);
-            boxInfo.setAttribute("class", "box-info");
-
-            let marcaL = document.createElement("label");
-            boxInfo.appendChild(marcaL);
-            let txtNodeMarca = document.createTextNode(auto.marca + " " + auto.modelo);
-            marcaL.appendChild(txtNodeMarca);
-            marcaL.setAttribute("class", "marca");
-
-            let precio = document.createElement("label");
-            boxInfo.appendChild(precio);
-
-            let precioFormateado = auto.precio.toLocaleString();
-            let txtNodePrecio = document.createTextNode("$" + precioFormateado);
-            precio.appendChild(txtNodePrecio);
-            precio.setAttribute("class", "precio");
-
-            let infoAdicional = document.createElement("label");
-            boxInfo.appendChild(infoAdicional);
-
-            // Utilizamos los valores individuales de fecha, kilometraje y ubicación para cada automóvil
-            let infoText = document.createTextNode(`${auto.fecha} - ${auto.kilometraje},  ${auto.ubicacion}`);
-            infoAdicional.appendChild(infoText);
-            infoAdicional.setAttribute("class", "info-adicional");
-
-            let boxCorazon = document.createElement("div");
-            boximg.appendChild(boxCorazon);
-            boxCorazon.setAttribute("class", "box-corazon");
-
-            let iconoCorazon = document.createElement("i");
-            boxCorazon.appendChild(iconoCorazon);
-            iconoCorazon.setAttribute("class", "icon-corazon fa-regular fa-heart");
-
-            // Agregamos un evento de clic al icono del corazón
-            boxCorazon.addEventListener("click", function () {
-                if (auto.favorito) {
-                    iconoCorazon.classList.remove("fas");
-                    iconoCorazon.classList.add("far");
-                    auto.favorito = false;
-                } else {
-                    iconoCorazon.classList.remove("far");
-                    iconoCorazon.classList.add("fas");
-                    auto.favorito = true;
-                }
-            });
-
-            let lineaDiv = document.createElement("hr");
-            boxproducto.appendChild(lineaDiv);
-            lineaDiv.setAttribute("class", "linea-div");
         }
-    });
-}
+        else if (inputUser == "chevrolet") {
+            cargarVehiculo(auto4);
 
-window.addEventListener("load", function () {
-    mostrarProductos(''); // Mostrar todos los productos al cargar la página
-
-    inputBusqueda.addEventListener('input', function () {
-        mostrarProductos(inputBusqueda.value);
-    });
+        }
+    }     
 });
+/* Función  con parámetro*/
+function cargarVehiculo(auto) {
+    let boxProductos = document.getElementById("boxProductos");
 
-//Filtros
-function cargarFiltro(){
-let boxFiltro = document.createElement("div");
-mainContent.appendChild(boxFiltro);
-boxFiltro.setAttribute("class", "box-filtro");
+    /* Caja para el producto */
+    let boxProducto = document.createElement("div");
+    boxProductos.appendChild(boxProducto);
+    boxProducto.setAttribute("class", "box-producto");
+
+    /* Caja para la imagen */
+    let boxImg = document.createElement("div");
+    boxProducto.appendChild(boxImg);
+    boxImg.setAttribute("class", "box-img");
+    let imgAuto = document.createElement("img");
+    boxImg.appendChild(imgAuto);
+    imgAuto.setAttribute("src", auto.imagen);
+    imgAuto.setAttribute("class", "img-auto");
+
+    /* Caja para la información */
+    let boxInfo = document.createElement("div");
+    boxProducto.appendChild(boxInfo);
+    boxInfo.setAttribute("class", "box-info");
+    let marca = document.createElement("label");
+    boxInfo.appendChild(marca);
+    let txtNodeMarca = document.createTextNode(auto.marca + " " + auto.modelo);
+    marca.appendChild(txtNodeMarca);
+    marca.setAttribute("class", "marca");
+    /* Precio */
+    let precio = document.createElement("label");
+    boxInfo.appendChild(precio);
+    let txtNodePrecio = document.createTextNode("$" + Intl.NumberFormat("de-DE").format(auto.precio));
+    precio.appendChild(txtNodePrecio);
+    precio.setAttribute("class", "precio");
+    /* año */
+    let ao = document.createElement("label");
+    boxInfo.appendChild(ao);
+    let txtNodeAnyo = document.createTextNode(auto.ao + " · ");
+    ao.appendChild(txtNodeAnyo);
+    ao.setAttribute("class", "info-ao");
+    /* Kilometros */
+    let kilometros = document.createElement("label");
+    boxInfo.appendChild(kilometros);
+    let kilometrosStr = Intl.NumberFormat("de-DE").format(auto.kilometros);
+    let txtNodeKilometros = document.createTextNode(kilometrosStr + " km" + " · ");
+
+    kilometros.appendChild(txtNodeKilometros);
+    kilometros.setAttribute("class", "info-kilometros");
+    /* Ciudad  */
+    let ciudad = document.createElement("label");
+    boxInfo.appendChild(ciudad);
+    let txtNodeCiudad = document.createTextNode(auto.ciudad);
+    ciudad.appendChild(txtNodeCiudad);
+    ciudad.setAttribute("class", "info-ciudad");
+
+    /* ícono corazón */
+    let boxCorazon = document.createElement("div");
+    boxImg.appendChild(boxCorazon);
+    let iconoCorazon = document.createElement("i");
+    boxCorazon.appendChild(iconoCorazon);
+    boxCorazon.setAttribute("class", "box-corazon");
+    iconoCorazon.setAttribute("class", "icon-corazon fa-regular fa-heart");
+
+    /* Línea divisora */
+    let lineaDiv = document.createElement("div");
+    boxProducto.appendChild(lineaDiv);
+    lineaDiv.setAttribute("class", "linea-div");
 }
-/*toggle*/
+
+/* FILTROS */
+cargarFiltro();
+
+function cargarFiltro() {
+    
+    mainContent.appendChild(boxFiltro);
+    boxFiltro.setAttribute("class", "box-filtro");
+}
+
+/* Toggle */
 let boxToggle = document.getElementById("boxToggle");
 let buttonToggle = document.getElementById("buttonToggle");
 
 boxToggle.addEventListener("click", () => {
- if(boxToggle.classList.contains("box-toggle-off")){
-    boxToggle.classList.add("box-toggle-on");
-    boxToggle.classList.remove("box-toggle-off");
-    buttonToggle.classList.add("button-toggle-on");
-    buttonToggle.classList.remove("button-toggle-off");
- }
- else{
-    boxToggle.classList.remove("box-toggle-on");
-    boxToggle.classList.add("box-toggle-off");
-    buttonToggle.classList.remove("button-toggle-on");
-    buttonToggle.classList.add("button-toggle-off");
- }
-})
+    if (boxToggle.classList.contains("box-toggle-off")) {
+        boxToggle.classList.add("box-toggle-on");
+        boxToggle.classList.remove("box-toggle-off");
+        buttonToggle.classList.add("button-toggle-on");
+        buttonToggle.classList.remove("button-toggle-off");
+    }
+    else {
+        boxToggle.classList.remove("box-toggle-on");
+        boxToggle.classList.add("box-toggle-off");
+        buttonToggle.classList.remove("button-toggle-on");
+        buttonToggle.classList.add("button-toggle-off");
+    }
+});
